ee0ab1ad00b165a8406e8cb905d3f965753b3633
yac <yac@blesmrt.net>
2016-06-03 06:42:26 +0200

    Render all the web pages
    Root page redirects to the first one

--- aiohex/__init__.py
+++ aiohex/__init__.py
@@ -6,15 +6,39 @@ from aiohttp import web
 
 __version__ = '0.1.0'
 
-async def view(r):
+def mk_body(pn):
   """
-  :type r: web.Request
-  :rtype: web.Response
+  :param pn: page number
+  :param pn: int
+
+  :returns: http response body
+  :rtype: bytes
   """
-  return web.Response(body = b"Hello World")
+  return '<html><head></head><body><h1>{}</h1><p>{}</p></body></html>'.format(
+    pn
+  , "".join(['<a href="/{0}">{0}</a>'.format(x) for x in range(1,4)])
+  ).encode("utf-8")
+
+async def page_view(r):
+  """
+  :returns: response for one of the 3 available pages
+  """
+  try:
+    pn = int(r.match_info['pn'])
+  except ValueError:
+    raise web.HTTPNotFound
+
+  return web.Response(body = mk_body(pn))
+
+async def root_view(r):
+  """
+  Redirects to first page
+  """
+  raise web.HTTPFound('/1')
 
 def main():
   app = web.Application()
-  app.router.add_route('GET', '/', view)
+  app.router.add_route('GET', '/'    , root_view)
+  app.router.add_route('GET', '/{pn}', page_view)
   web.run_app(app)
   sys.exit(0)
